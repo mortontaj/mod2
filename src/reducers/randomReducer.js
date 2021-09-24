bea49245eeadd697abe2de50b0e0286664355241
unknown <mortontaj@gmail.com>
2021-09-24 08:19:39 -0400

    updated

--- src/reducers/randomReducer.js
+++ src/reducers/randomReducer.js
@@ -5,17 +5,18 @@ const initialState = {
 export default function randomReducer(state = initialState, action) {
   switch (action.type) {
     case "Deposit":
-      return { marketValue: state.marketValue + 10000 };
+      return { marketValue: Number(state.marketValue + 10000) };
     case "Withdraw":
       if (state.marketValue <= 10000) return { marketValue: 0 };
-      else return { marketValue: state.marketValue - 10000 };
+      else return { marketValue: Number(state.marketValue - 10000) };
     case "Speculate":
       return {
-        marketValue:
+        marketValue: Number(
           state.marketValue +
-          state.marketValue *
-            0.0697303 *
-            (Math.random() ** 0.5138 - Math.random() ** (1 - 0.5138)),
+            state.marketValue *
+              0.0697303 *
+              (Math.random() ** 0.5138 - Math.random() ** (1 - 0.5138))
+        ),
       };
     default:
       return { marketValue: state.marketValue };
--- src/reducers/strategyReducer.js
+++ src/reducers/strategyReducer.js
@@ -5,17 +5,16 @@ const initialState2 = {
 export default function strategyReducer(state2 = initialState2, action) {
   switch (action.type) {
     case "Deposit2":
-      return { marketValue2: state2.marketValue2 + 10000 };
+      return { marketValue2: Number(state2.marketValue2 + 10000) };
     case "Withdraw2":
       if (state2.marketValue2 <= 10000) return { marketValue2: 0 };
-      else return { marketValue2: state2.marketValue2 - 10000 };
+      else return { marketValue2: Number(state2.marketValue2 - 10000) };
     case "Trade":
       let win = 0.9;
       if (Math.random() < win) {
         return {
-          marketValue2: (
-            state2.marketValue2 *
-            (1 + 0.01973 * Math.random())
+          marketValue2: Number(
+            state2.marketValue2 * (1 + 0.01973 * Math.random())
           ).toFixed(2),
         };
       } else
